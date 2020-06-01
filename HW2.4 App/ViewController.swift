5305ea12dd865608b94d50b469ee23e59dd919e0
Gennady Kureev <gennady008@gmail.com>
2020-06-01 16:12:51 +0300

    Initial Commit

---
+++ HW2.4 App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.4 App
+//
+//  Created by Kureev on 01.06.2020.
+//  Copyright © 2020 Kureev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
